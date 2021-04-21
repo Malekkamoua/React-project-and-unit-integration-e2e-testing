da969745ee2ee09a8291459dbec2ad5c11a56e50
skanderbennia <mohamedskander.bennia@gmail.com>
2021-04-21 12:17:30 +0100

    redux

--- src/index.js
+++ src/index.js
@@ -25,14 +25,21 @@ import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-      <Redirect from="/" to="/admin/index" />
-    </Switch>
-  </BrowserRouter>,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/admin' render={(props) => <AdminLayout {...props} />} />
+        <Route path='/auth' render={(props) => <AuthLayout {...props} />} />
+        <Redirect from='/' to='/admin/index' />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
