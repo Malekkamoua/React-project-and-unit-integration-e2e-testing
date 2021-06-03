40eaa86eea32117db367d373144607cb69d2318e
skanderbennia <mohamedskander.bennia@gmail.com>
2021-06-03 21:43:20 +0100

    fixed add student

--- src/views/examples/Students.js
+++ src/views/examples/Students.js
@@ -35,12 +35,14 @@ import {
   Input,
 } from "reactstrap";
 import api from "../../api";
+import ClipLoader from "react-spinners/ClipLoader";
 // core components
 import Header from "components/Headers/Header.js";
 import { addPfe, getPfe, updatePfe } from "../../services/pfeService";
 import { getEtudiant } from "../../services/studentService";
 
 const Students = () => {
+  const [loading, setLoading] = useState(false);
   const [nomPfe, setnomPfe] = useState("");
   const [contentPfe, setcontentPfe] = useState("");
   const [currentPfe, setCurrentPfe] = useState("");
@@ -48,17 +50,33 @@ const Students = () => {
   const systemDate = new Date();
   const [messageData, setMessageData] = useState();
   const { token, userInformation } = JSON.parse(localStorage.getItem("user"));
+
+  console.log(token);
   useEffect(async () => {
-    if (userInformation.pfe === undefined) {
-      const res = await getEtudiant(token, userInformation._id);
-      console.log(res);
-    } else {
+    if (userInformation.pfe !== undefined) {
+      console.log("i m here ------------------");
+      setLoading(true);
+      const res = await getPfe(token, userInformation.pfe);
+
+      console.log(res[0]);
+      setCurrentPfe(res[0]);
+      setcontentPfe(res[0].content ? res[0].content : "");
+      setnomPfe(res[0].title ? res[0].title : "");
+      setEndDate(res[0].year.endDate);
+      setLoading(false);
+    }
+  }, []);
+  useEffect(async () => {
+    if (userInformation.pfe !== undefined) {
+      console.log("i m here ------------------");
+      setLoading(true);
       const res = await getPfe(token, userInformation.pfe);
       console.log(res[0]);
       setCurrentPfe(res[0]);
       setcontentPfe(res[0].content ? res[0].content : "");
       setnomPfe(res[0].title ? res[0].title : "");
       setEndDate(res[0].year.endDate);
+      setLoading(false);
     }
   }, []);
   const addPfeHandler = async () => {
@@ -68,6 +86,15 @@ const Students = () => {
     );
     console.log(res);
     setCurrentPfe(res);
+    const resetUser = JSON.stringify({
+      ...JSON.parse(localStorage.getItem("user")),
+      userInformation: {
+        ...JSON.parse(localStorage.getItem("user")).userInformation,
+        pfe: res._id,
+      },
+    });
+    localStorage.removeItem("user");
+    localStorage.setItem("user", resetUser);
     setcontentPfe(res.content ? res.content : "");
     setnomPfe(res.title ? res.title : "");
     setEndDate(res.year.endDate);
@@ -94,92 +121,96 @@ const Students = () => {
       <Header />
       {/* Page content */}
       {new Date(endDate).getTime() !== systemDate.getTime() ? (
-        <Container className='mt--7' fluid>
-          {/* Table */}
-          <Row>
-            <div className='col'>
-              <Card className='shadow'>
-                <CardHeader className='border-0'>
-                  <h3 className='mb-0'>
-                    {currentPfe ? "ModifierPfe" : "Ajouter PFE"}
-                  </h3>
-                </CardHeader>
+        loading ? (
+          <ClipLoader loading={loading} />
+        ) : (
+          <Container className='mt--7' fluid>
+            {/* Table */}
+            <Row>
+              <div className='col'>
+                <Card className='shadow'>
+                  <CardHeader className='border-0'>
+                    <h3 className='mb-0'>
+                      {currentPfe ? "ModifierPfe" : "Ajouter PFE"}
+                    </h3>
+                  </CardHeader>
+                </Card>
+              </div>
+            </Row>
+            {/* Dark table */}
+            <Col lg='13' md='13'>
+              <Card className='bg-secondary shadow border-0'>
+                <CardBody className='px-lg-5 py-lg-5'>
+                  <Form role='form'>
+                    <FormGroup>
+                      <InputGroup className='input-group-alternative mb-3'>
+                        <InputGroupAddon addonType='prepend'>
+                          <InputGroupText>
+                            <i className='ni ni-hat-3' />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          placeholder='Nom PFE'
+                          type='text'
+                          value={nomPfe}
+                          onChange={(e) => {
+                            setnomPfe(e.target.value);
+                          }}
+                        />
+                      </InputGroup>
+                    </FormGroup>
+                    <FormGroup>
+                      <InputGroup className='input-group-alternative mb-3'>
+                        <InputGroupAddon addonType='prepend'>
+                          <InputGroupText>
+                            <i className='ni ni-email-83' />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                          placeholder='Content'
+                          type='textarea'
+                          value={contentPfe}
+                          onChange={(e) => {
+                            setcontentPfe(e.target.value);
+                          }}
+                        />
+                      </InputGroup>
+                    </FormGroup>
+                    {messageData && (
+                      <p style={{ color: messageData.color }}>
+                        {messageData.message}
+                      </p>
+                    )}
+                    <div className='text-center'>
+                      {!currentPfe && (
+                        <Button
+                          className='mt-4'
+                          color='primary'
+                          type='button'
+                          onClick={async () => await addPfeHandler()}
+                        >
+                          Ajouter PFE
+                        </Button>
+                      )}
+                    </div>
+                    <div className='text-center'>
+                      {currentPfe && (
+                        <Button
+                          className='mt-4'
+                          color='primary'
+                          type='button'
+                          onClick={async () => await updatePfeHandler()}
+                        >
+                          Modifier PFE
+                        </Button>
+                      )}
+                    </div>
+                  </Form>
+                </CardBody>
               </Card>
-            </div>
-          </Row>
-          {/* Dark table */}
-          <Col lg='13' md='13'>
-            <Card className='bg-secondary shadow border-0'>
-              <CardBody className='px-lg-5 py-lg-5'>
-                <Form role='form'>
-                  <FormGroup>
-                    <InputGroup className='input-group-alternative mb-3'>
-                      <InputGroupAddon addonType='prepend'>
-                        <InputGroupText>
-                          <i className='ni ni-hat-3' />
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        placeholder='Nom PFE'
-                        type='text'
-                        value={nomPfe}
-                        onChange={(e) => {
-                          setnomPfe(e.target.value);
-                        }}
-                      />
-                    </InputGroup>
-                  </FormGroup>
-                  <FormGroup>
-                    <InputGroup className='input-group-alternative mb-3'>
-                      <InputGroupAddon addonType='prepend'>
-                        <InputGroupText>
-                          <i className='ni ni-email-83' />
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input
-                        placeholder='Content'
-                        type='textarea'
-                        value={contentPfe}
-                        onChange={(e) => {
-                          setcontentPfe(e.target.value);
-                        }}
-                      />
-                    </InputGroup>
-                  </FormGroup>
-                  {messageData && (
-                    <p style={{ color: messageData.color }}>
-                      {messageData.message}
-                    </p>
-                  )}
-                  <div className='text-center'>
-                    {!currentPfe && (
-                      <Button
-                        className='mt-4'
-                        color='primary'
-                        type='button'
-                        onClick={async () => await addPfeHandler()}
-                      >
-                        Ajouter PFE
-                      </Button>
-                    )}
-                  </div>
-                  <div className='text-center'>
-                    {currentPfe && (
-                      <Button
-                        className='mt-4'
-                        color='primary'
-                        type='button'
-                        onClick={async () => await updatePfeHandler()}
-                      >
-                        Modifier PFE
-                      </Button>
-                    )}
-                  </div>
-                </Form>
-              </CardBody>
-            </Card>
-          </Col>
-        </Container>
+            </Col>
+          </Container>
+        )
       ) : (
         ""
       )}
