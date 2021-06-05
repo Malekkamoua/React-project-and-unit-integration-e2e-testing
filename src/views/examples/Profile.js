59dc973561b95a9d7f1528a38397c4edd6d5fa8e
skanderbennia <mohamedskander.bennia@gmail.com>
2021-06-05 20:29:08 +0100

    profil teacher etudiant changed

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -28,7 +28,7 @@ import {
   Input,
   Container,
   Row,
-  Col
+  Col,
 } from "reactstrap";
 // core components
 import UserHeader from "components/Headers/UserHeader.js";
@@ -40,62 +40,158 @@ const Profile = () => {
     <>
       <UserHeader />
       {/* Page content */}
-      <Container className="mt--7" fluid>
+      <Container className='mt--7' fluid>
         <Row>
-          <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
-            <Card className="card-profile shadow">
-              <Row className="justify-content-center">
-                <Col className="order-lg-2" lg="3">
-                  <div className="card-profile-image">
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
+          <Col className='order-xl-2 mb-5 mb-xl-0' xl='4'>
+            <Card className='card-profile shadow'>
+              <Row className='justify-content-center'>
+                <Col className='order-lg-2' lg='3'>
+                  <div className='card-profile-image'>
+                    <a href='#pablo' onClick={(e) => e.preventDefault()}>
                       <img
-                        alt="..."
-                        className="rounded-circle"
-                        src="https://robohash.org/e27c051ad572320f5abb4c9b0bffb9d8?set=set3&bgset=bg2&size=200x200"
+                        alt='...'
+                        className='rounded-circle'
+                        src='https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/user.png'
                       />
                     </a>
                   </div>
                 </Col>
               </Row>
-              <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                <div className="d-flex justify-content-between"></div>
+              <CardHeader className='text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4'>
+                <div className='d-flex justify-content-between'></div>
               </CardHeader>
-              <CardBody className="pt-0 pt-md-4">
+              <CardBody className='pt-0 pt-md-4'>
                 <Row>
-                  <div className="col">
-                    <div className="card-profile-stats d-flex justify-content-center mt-md-5">
+                  <div className='col'>
+                    <div className='card-profile-stats d-flex justify-content-center mt-md-5'>
                       <div></div>
                     </div>
                   </div>
                 </Row>
-                <div className="text-center">
+                <div className='text-center'>
                   <h3>
                     {user.name}
-                    <span className="font-weight-light">, 27</span>
+                    <span className='font-weight-light'>, {user.age}</span>
                   </h3>
-                  <div className="h5 font-weight-300">
-                    <i className="ni location_pin mr-2" />
-                    Tunisie,Tunis
+                  <div className='h5 font-weight-300'>
+                    <i className='ni location_pin mr-2' />
+                    Tunisia
                   </div>
-                  <div className="h5 mt-4">
-                    <i className="ni business_briefcase-24 mr-2" />
-                    {user.role === "teacher"
-                      ? "Professeur"
-                      : user.role === "student"
-                      ? "Etudiant"
-                      : ""}
+                  <div className='h5 mt-4'>
+                    <i className='ni business_briefcase-24 mr-2' />
+                    {user.role == "teacher" && "Professeur"}{" "}
+                    {user.role == "student" && "Etudiant"}
                   </div>
                   <div>
-                    <i className="ni education_hat mr-2" />
-                    Université de Manouba
+                    <i className='ni education_hat mr-2' />
+                    ISAMM - Université de la Manouba
                   </div>
-                  <hr className="my-4" />
-                  <p>
-                    Ryan — the name taken by Melbourne-raised, Brooklyn-based
-                    Nick Murphy — writes, performs and records all of his own
-                    music.
-                  </p>
                 </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col className='order-xl-1' xl='8'>
+            <Card className='bg-secondary shadow'>
+              <CardHeader className='bg-white border-0'>
+                <Row className='align-items-center'>
+                  <Col xs='8'>
+                    <h3 className='mb-0'>
+                      Details {user.role == "teacher" && "Professeur"}{" "}
+                      {user.role == "student" && "Etudiant"}
+                    </h3>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  <h6 className='heading-small text-muted mb-4'>
+                    Informations {user.role == "teacher" && "Professeur"}{" "}
+                    {user.role == "student" && "Etudiant"}
+                  </h6>
+                  <div className='pl-lg-4'>
+                    <Row>
+                      <Col lg='6'>
+                        <FormGroup>
+                          <label
+                            className='form-control-label'
+                            htmlFor='input-email'
+                          >
+                            Email
+                          </label>
+                          <Input
+                            className='form-control-alternative'
+                            id='input-email'
+                            placeholder='jesse@example.com'
+                            type='email'
+                            disabled
+                            value={user.email}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col lg='6'></Col>
+                    </Row>
+                    <Row>
+                      <Col lg='6'>
+                        <FormGroup>
+                          <label
+                            className='form-control-label'
+                            htmlFor='input-first-name'
+                          >
+                            Prename
+                          </label>
+                          <Input
+                            className='form-control-alternative'
+                            defaultValue='Lucky'
+                            id='input-first-name'
+                            type='text'
+                            disabled
+                            value={user.name.split(" ")[1]}
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col lg='6'>
+                        <FormGroup>
+                          <label
+                            className='form-control-label'
+                            htmlFor='input-last-name'
+                          >
+                            name
+                          </label>
+                          <Input
+                            className='form-control-alternative'
+                            defaultValue='Jesse'
+                            id='input-last-name'
+                            placeholder='Last name'
+                            type='text'
+                            disabled
+                            value={user.name.split(" ")[0]}
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col lg='6'>
+                        {/* <FormGroup>
+                          <label
+                            className='form-control-label'
+                            htmlFor='input-first-name'
+                          >
+                            Sexe
+                          </label>
+                          <Input
+                            className='form-control-alternative'
+                            defaultValue='Lucky'
+                            id='input-first-name'
+                            placeholder='First name'
+                            type='text'
+                            value={student.sexe}
+                          />
+                        </FormGroup> */}
+                      </Col>
+                    </Row>
+                  </div>
+                </Form>
+                <br></br> <br></br>
               </CardBody>
             </Card>
           </Col>
