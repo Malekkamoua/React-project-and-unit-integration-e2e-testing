f9c545e7322d8def1ed2a92cc00756d309e5fbd5
Malekkamoua <malekkamoua50@gmail.com>
2021-06-16 00:41:20 +0200

    e2e updates

--- cypress/e2e/Login_AddPfe.test.js
+++ cypress/e2e/Login_AddPfe.test.js
@@ -1,10 +1,8 @@
-describe("My first test", () => {
-  it("Doesnt do much", () => {
-    expect(true).to.equal(true);
-  });
-  it.only("Failed login", () => {
+describe("E2E Testing ", () => {
+  it("Echec de connexion", () => {
     cy.visit("/");
     cy.wait(2000);
+
     cy.get(".mb-3 > .input-group-alternative > .form-control").type(
       "malekkamoua50@gmail.com"
     );
@@ -13,26 +11,19 @@ describe("My first test", () => {
     );
     cy.get(".my-4").click();
     cy.url().should("include", "/login");
+
+    cy.wait(3000);
   });
-  it("Loads with success", () => {
+  it("Login + Ajout PFE", () => {
     cy.visit("/");
     cy.wait(2000);
     cy.get(".mb-3 > .input-group-alternative > .form-control").type(
-      "malekkamoua50@gmail.com"
+      "student@gmail.com"
     );
     cy.get(":nth-child(2) > .input-group-alternative > .form-control").type(
-      "etudiants"
+      "student2"
     );
     cy.get(".my-4").click();
-
-    cy.get(".mb-3 > .input-group-alternative > .form-control").should(
-      "have.value",
-      "malekkamoua50@gmail.com"
-    );
-    cy.get(":nth-child(2) > .input-group-alternative > .form-control").type(
-      "etudiants"
-    );
-    cy.get("p").should("have.value", "incorrect email or password");
 
     cy.wait(3000);
     cy.get(":nth-child(3) > .nav-link").click();
@@ -41,8 +32,11 @@ describe("My first test", () => {
       "Added this title from cypress wohooo"
     );
     cy.get(":nth-child(2) > .input-group-alternative > .form-control").type(
-      "Section 1.10.32 of , written by Cicero in 45 BC Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      "Section 1.10.32 of , written by Cicero in 45 BC Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam."
     );
-    cy.get(":nth-child(3) > .mt-4").click();
+    cy.get("form > :nth-child(4) > .mt-4").click();
+
+    // cy.wait(3000);
+    // cy.get(".navbar-nav > :nth-child(2) > .nav-link").click();
   });
 });
