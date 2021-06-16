9d817b7a9fd88bf1ee5c371cbdaefdae2ba19ed6
Malekkamoua <malekkamoua50@gmail.com>
2021-06-16 07:29:14 +0200

    Test updates

--- cypress/e2e/Login_AddPfe.test.js
+++ cypress/e2e/Login_AddPfe.test.js
@@ -18,10 +18,10 @@ describe("E2E Testing ", () => {
     cy.visit("/");
     cy.wait(2000);
     cy.get(".mb-3 > .input-group-alternative > .form-control").type(
-      "student@gmail.com"
+      "cy@gmail.com"
     );
     cy.get(":nth-child(2) > .input-group-alternative > .form-control").type(
-      "student2"
+      "cypress"
     );
     cy.get(".my-4").click();
 
@@ -29,14 +29,14 @@ describe("E2E Testing ", () => {
     cy.get(":nth-child(3) > .nav-link").click();
 
     cy.get(":nth-child(1) > .input-group-alternative > .form-control").type(
-      "Added this title from cypress wohooo"
+      "Added this title from cypress"
     );
     cy.get(":nth-child(2) > .input-group-alternative > .form-control").type(
       "Section 1.10.32 of , written by Cicero in 45 BC Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam."
     );
-    cy.get("form > :nth-child(4) > .mt-4").click();
+    cy.get(":nth-child(3) > .mt-4").click();
 
-    // cy.wait(3000);
-    // cy.get(".navbar-nav > :nth-child(2) > .nav-link").click();
+    cy.wait(3000);
+    cy.get(".navbar-nav > :nth-child(2) > .nav-link").click();
   });
 });
--- src/views/examples/DetailEtudiant.js
+++ src/views/examples/DetailEtudiant.js
@@ -29,7 +29,7 @@ import {
   Input,
   Container,
   Row,
-  Col,
+  Col
 } from "reactstrap";
 // core components
 import UserHeader from "components/Headers/UserHeader.js";
@@ -77,133 +77,133 @@ const DetailStudent = ({ studentprops }) => {
     <>
       <UserHeader />
       {/* Page content */}
-      <Container className='mt--7' fluid>
+      <Container className="mt--7" fluid>
         <Row>
-          <Col className='order-xl-2 mb-5 mb-xl-0' xl='4'>
-            <Card className='card-profile shadow'>
-              <Row className='justify-content-center'>
-                <Col className='order-lg-2' lg='3'>
-                  <div className='card-profile-image'>
-                    <a href='#pablo' onClick={(e) => e.preventDefault()}>
+          <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
+            <Card className="card-profile shadow">
+              <Row className="justify-content-center">
+                <Col className="order-lg-2" lg="3">
+                  <div className="card-profile-image">
+                    <a href="#pablo" onClick={e => e.preventDefault()}>
                       <img
-                        alt='...'
-                        className='rounded-circle'
-                        src='https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/user.png'
+                        alt="..."
+                        className="rounded-circle"
+                        src="https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/user.png"
                       />
                     </a>
                   </div>
                 </Col>
               </Row>
-              <CardHeader className='text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4'>
-                <div className='d-flex justify-content-between'></div>
+              <CardHeader className="text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                <div className="d-flex justify-content-between"></div>
               </CardHeader>
-              <CardBody className='pt-0 pt-md-4'>
+              <CardBody className="pt-0 pt-md-4">
                 <Row>
-                  <div className='col'>
-                    <div className='card-profile-stats d-flex justify-content-center mt-md-5'>
+                  <div className="col">
+                    <div className="card-profile-stats d-flex justify-content-center mt-md-5">
                       <div></div>
                     </div>
                   </div>
                 </Row>
-                <div className='text-center'>
+                <div className="text-center">
                   <h3>
                     {student.name}
-                    <span className='font-weight-light'>, {student.age}</span>
+                    <span className="font-weight-light">, {student.age}</span>
                   </h3>
-                  <div className='h5 font-weight-300'>
-                    <i className='ni location_pin mr-2' />
+                  <div className="h5 font-weight-300">
+                    <i className="ni location_pin mr-2" />
                     Tunisia
                   </div>
-                  <div className='h5 mt-4'>
-                    <i className='ni business_briefcase-24 mr-2' />
+                  <div className="h5 mt-4">
+                    <i className="ni business_briefcase-24 mr-2" />
                     Etudiant
                   </div>
                   <div>
-                    <i className='ni education_hat mr-2' />
+                    <i className="ni education_hat mr-2" />
                     ISAMM - Université de la Manouba
                   </div>
                 </div>
               </CardBody>
             </Card>
           </Col>
-          <Col className='order-xl-1' xl='8'>
-            <Card className='bg-secondary shadow'>
-              <CardHeader className='bg-white border-0'>
-                <Row className='align-items-center'>
-                  <Col xs='8'>
-                    <h3 className='mb-0'>Details Etudiant</h3>
+          <Col className="order-xl-1" xl="8">
+            <Card className="bg-secondary shadow">
+              <CardHeader className="bg-white border-0">
+                <Row className="align-items-center">
+                  <Col xs="8">
+                    <h3 className="mb-0">Details Etudiant</h3>
                   </Col>
                 </Row>
               </CardHeader>
               <CardBody>
                 <Form>
-                  <h6 className='heading-small text-muted mb-4'>
+                  <h6 className="heading-small text-muted mb-4">
                     Informations de l'étudiant
                   </h6>
-                  <div className='pl-lg-4'>
+                  <div className="pl-lg-4">
                     <Row>
-                      <Col lg='6'>
+                      <Col lg="6">
                         <FormGroup>
                           <label
-                            className='form-control-label'
-                            htmlFor='input-email'
+                            className="form-control-label"
+                            htmlFor="input-email"
                           >
                             Email
                           </label>
                           <Input
-                            className='form-control-alternative'
-                            id='input-email'
-                            placeholder='jesse@example.com'
-                            type='email'
+                            className="form-control-alternative"
+                            id="input-email"
+                            placeholder="jesse@example.com"
+                            type="email"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            onChange={e => setEmail(e.target.value)}
                           />
                         </FormGroup>
                       </Col>
-                      <Col lg='6'></Col>
+                      <Col lg="6"></Col>
                     </Row>
                     <Row>
-                      <Col lg='6'>
+                      <Col lg="6">
                         <FormGroup>
                           <label
-                            className='form-control-label'
-                            htmlFor='input-first-name'
+                            className="form-control-label"
+                            htmlFor="input-first-name"
                           >
-                            Prename
+                            Prenom
                           </label>
                           <Input
-                            className='form-control-alternative'
-                            defaultValue='Lucky'
-                            id='input-first-name'
-                            placeholder='First name'
-                            type='text'
+                            className="form-control-alternative"
+                            defaultValue="Lucky"
+                            id="input-first-name"
+                            placeholder="First name"
+                            type="text"
                             value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
+                            onChange={e => setFirstName(e.target.value)}
                           />
                         </FormGroup>
                       </Col>
-                      <Col lg='6'>
+                      <Col lg="6">
                         <FormGroup>
                           <label
-                            className='form-control-label'
-                            htmlFor='input-last-name'
+                            className="form-control-label"
+                            htmlFor="input-last-name"
                           >
-                            name
+                            Nom
                           </label>
                           <Input
-                            className='form-control-alternative'
-                            defaultValue='Jesse'
-                            id='input-last-name'
-                            placeholder='Last name'
-                            type='text'
+                            className="form-control-alternative"
+                            defaultValue="Jesse"
+                            id="input-last-name"
+                            placeholder="Last name"
+                            type="text"
                             value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
+                            onChange={e => setLastName(e.target.value)}
                           />
                         </FormGroup>
                       </Col>
                     </Row>
                     <Row>
-                      <Col lg='6'>
+                      <Col lg="6">
                         {/* <FormGroup>
                           <label
                             className='form-control-label'
@@ -225,16 +225,16 @@ const DetailStudent = ({ studentprops }) => {
                   </div>
                   <Col
                     style={{ position: "relative", left: "70%" }}
-                    className='text-right'
-                    xs='4'
+                    className="text-right"
+                    xs="4"
                   >
                     <p style={{ color: "green" }}>{message}</p>
                     <Button
-                      color='primary'
+                      color="primary"
                       onClick={() => {
                         updateStudentHandler();
                       }}
-                      size='sm'
+                      size="sm"
                     >
                       Sauvegarder
                     </Button>
@@ -242,68 +242,68 @@ const DetailStudent = ({ studentprops }) => {
                 </Form>
                 <br></br> <br></br>
                 <Form>
-                  <h6 className='heading-small text-muted mb-4'>
+                  <h6 className="heading-small text-muted mb-4">
                     Informations du projet
                   </h6>
-                  <div className='pl-lg-4'>
+                  <div className="pl-lg-4">
                     <Row>
-                      <Col lg='6'>
+                      <Col lg="6">
                         <FormGroup>
                           <label
-                            className='form-control-label'
-                            htmlFor='input-first-name'
+                            className="form-control-label"
+                            htmlFor="input-first-name"
                           >
                             Titre
                           </label>
                           <Input
-                            className='form-control-alternative'
-                            defaultValue='Lucky'
-                            id='input-first-name'
-                            placeholder='Titre pfe'
-                            type='text'
+                            className="form-control-alternative"
+                            defaultValue="Lucky"
+                            id="input-first-name"
+                            placeholder="Titre pfe"
+                            type="text"
                             value={namePfe}
                             disabled
-                            onChange={(e) => setnamePfe(e.target.value)}
+                            onChange={e => setnamePfe(e.target.value)}
                           />
                         </FormGroup>
                       </Col>
-                      <Col lg='6'>
+                      <Col lg="6">
                         <FormGroup>
                           <label
-                            className='form-control-label'
-                            htmlFor='input-last-name'
+                            className="form-control-label"
+                            htmlFor="input-last-name"
                           >
                             Nom de l'encadrant
                           </label>
                           <Input
-                            className='form-control-alternative'
-                            defaultValue='Jesse'
-                            id='input-last-name'
+                            className="form-control-alternative"
+                            defaultValue="Jesse"
+                            id="input-last-name"
                             placeholder="Nom de l'encadrant"
-                            type='text'
+                            type="text"
                             value={nameProf}
                             disabled
-                            onChange={(e) => setnameProf(e.target.value)}
+                            onChange={e => setnameProf(e.target.value)}
                           />
                         </FormGroup>
                       </Col>
                     </Row>
 
                     <Row>
-                      <Col lg='12'>
+                      <Col lg="12">
                         <FormGroup>
                           <label
-                            className='form-control-label'
-                            htmlFor='input-first-name'
+                            className="form-control-label"
+                            htmlFor="input-first-name"
                           >
                             Contenu
                           </label>
                           <Input
-                            className='form-control-alternative'
-                            defaultValue='Contenu du pfe'
-                            id='input-first-name'
-                            type='textarea'
-                            rows='50'
+                            className="form-control-alternative"
+                            defaultValue="Contenu du pfe"
+                            id="input-first-name"
+                            type="textarea"
+                            rows="50"
                             value={contentPfe}
                             disabled
                           />
@@ -311,7 +311,7 @@ const DetailStudent = ({ studentprops }) => {
                       </Col>
                     </Row>
                     <Row>
-                      <Col lg='6'>
+                      <Col lg="6">
                         {/* <FormGroup>
                         <label
                           className='form-control-label'
@@ -340,7 +340,7 @@ const DetailStudent = ({ studentprops }) => {
     </>
   );
 };
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return { studentprops: state.activeStudent };
 };
 export default connect(mapStateToProps)(DetailStudent);
